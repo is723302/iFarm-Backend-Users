587523780569f90ea1ef63113cfe5c4646d08674
Julieta <julietaramosayon@gmail.com>
2021-05-09 20:34:31 -0500

    update user controller

--- src/utils/schemas/user.js
+++ src/utils/schemas/user.js
@@ -13,7 +13,7 @@ const greenhousesIdSchema = joi.array();
 const createUserSchema = {
     email: userEmailSchema.required(),
     name: userNameSchema.required(),
-    password: userPasswordSchema.required(),
+    password: userPasswordSchema,
     role: userRoleSchema.required(),
     google_id: userGoogleIdSchema,
     profile_pic: userProfilePicSchema,
@@ -26,6 +26,7 @@ const updateUserSchema = {
     name: userNameSchema,
     password: userPasswordSchema,
     role: userRoleSchema,
+    google_id: userGoogleIdSchema,
     profile_pic: userProfilePicSchema,
     supervisor_id: supervisorIdSchema,
     greenhouses_id: greenhousesIdSchema,
